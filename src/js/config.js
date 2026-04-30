c2be2a27445d588cd8cb7f150d0c5d934460779f
huluvu424242 <huluvu424242@gmail.com>
2026-04-30 19:50:09 +0200

    [setup] initiales gerüst konfiguriert

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 window.PRESENCE_CONFIG = {
   githubApiBaseUrl: 'https://api.github.com',
-  owner: 'DEIN_GITHUB_USER_ODER_ORG',
+  owner: 'huluvu424242',
   repo: 'presence-board',
   issueTitlePrefix: 'presence-',
   statusTtlMinutes: 540,
